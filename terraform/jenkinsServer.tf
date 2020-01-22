82adce66db8292a3e0ffb4b8c42d2343001ab33f
woodchuckchoi <choiggoggi@gmail.com>
2020-01-23 00:49:51 +0900

    terraform script edited

--- terraform/jenkinsServer.tf
+++ terraform/jenkinsServer.tf
@@ -4,16 +4,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "jenkins_server" {
-  ami           = "ami-00379ec40a3e30f87"
+  ami           = "landingProject-JenkinsServer"
   instance_type = "t2.micro"
   vpc_security_group_ids = ["sg-03565efa99637b5d1"]
-
-  provisioner "file" {
-    source      = "init.sh"
-    destination = "init.sh"
-  }
-
-  provisioner "local-exec" {
-    command = "sh init.sh"
-  }
 }
