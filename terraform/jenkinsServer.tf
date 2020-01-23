006cb21aeb7d21e06ce933bf5813463c1aff3afa
woodchuckchoi <choiggoggi@gmail.com>
2020-01-23 09:29:22 +0900

    minor changes on jenkinsServer terraform file

--- terraform/jenkinsServer.tf
+++ terraform/jenkinsServer.tf
@@ -4,7 +4,8 @@ provider "aws" {
 }
 
 resource "aws_instance" "jenkins_server" {
-  ami           = "landingProject-JenkinsServer"
+  ami           = "ami-0b9734b974d49e8a9"
   instance_type = "t2.micro"
   vpc_security_group_ids = ["sg-03565efa99637b5d1"]
+  key_name = "jenkins_server"
 }
