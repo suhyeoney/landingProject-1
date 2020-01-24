b58ad818346eb5236ea4fa446d289e451f890f15
woodchuckchoi <choiggoggi@gmail.com>
2020-01-24 22:55:54 +0900

    jenkins,terraform provisioned server(EC2) automation done

--- jenkinsServer/terraform/jenkinsServer.tf
+++ jenkinsServer/terraform/jenkinsServer.tf
@@ -1,10 +1,14 @@
+variable "image_id" {
+  type = string
+}
+
 provider "aws" {
   profile = "default"
   region  = "ap-northeast-2"
 }
 
 resource "aws_instance" "jenkins_server" {
-  ami           = "ami-016351c2ca998ee89"
+  ami           = var.image_id
   instance_type = "t2.micro"
   vpc_security_group_ids = ["sg-03565efa99637b5d1"]
   key_name = "jenkins_server"
